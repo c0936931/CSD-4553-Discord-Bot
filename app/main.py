63a60c3079226a2aee6dc54624f474169a86fd5b
c0936931 <c0936931@mylambton.ca>
2026-04-22 08:32:59 -0400

    Update main.py
    Enable logging again

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from db import Database
 import logging
 
-# from logger import DiscordLogHandler
+from logger import DiscordLogHandler
 
 load_dotenv()
 
@@ -52,12 +52,12 @@ def main():
     DISCORD_TOKEN = str(require_env("DISCORD_TOKEN"))
 
     # Optional - If missing skip logging
-    # LOG_CHANNEL = os.getenv("LOG_CHANNEL")
-    # CHANNEL_LOG_LEVEL = os.getenv("CHANNEL_LOG_LEVEL", "INFO").upper()
+    LOG_CHANNEL = os.getenv("LOG_CHANNEL")
+    CHANNEL_LOG_LEVEL = os.getenv("CHANNEL_LOG_LEVEL", "INFO").upper()
 
-    # if LOG_CHANNEL:
-    # 	LOG_CHANNEL = int(LOG_CHANNEL)
-    # 	CHANNEL_LOG_LEVEL = getattr(logging, CHANNEL_LOG_LEVEL, logging.INFO)
+    if LOG_CHANNEL:
+    	LOG_CHANNEL = int(LOG_CHANNEL)
+    	CHANNEL_LOG_LEVEL = getattr(logging, CHANNEL_LOG_LEVEL, logging.INFO)
 
     # Bot setup
     intents = discord.Intents.default()
@@ -68,29 +68,29 @@ def main():
     db = Database(MONGO_URI)
     bot.db = db  # attach db to bot so cogs can access it via bot.db
 
-    # if LOG_CHANNEL:
-    # 	# Set root log level
-    # 	logging.getLogger().setLevel(logging.DEBUG)
+    if LOG_CHANNEL:
+    	# Set root log level
+    	logging.getLogger().setLevel(logging.DEBUG)
 
-    # 	discord_handler = DiscordLogHandler(
-    # 		bot=bot,
-    # 		channel_id=LOG_CHANNEL,
-    # 		level=CHANNEL_LOG_LEVEL
-    # 	)
+    	discord_handler = DiscordLogHandler(
+    		bot=bot,
+    		channel_id=LOG_CHANNEL,
+    		level=CHANNEL_LOG_LEVEL
+    	)
 
-    # 	formatter = logging.Formatter("[%(levelname)s] %(message)s")
-    # 	discord_handler.setFormatter(formatter)
+    	formatter = logging.Formatter("[%(levelname)s] %(message)s")
+    	discord_handler.setFormatter(formatter)
 
-    # 	# Attach to root logger (your logs)
-    # 	root_logger = logging.getLogger()
-    # 	root_logger.addHandler(discord_handler)
+    	# Attach to root logger (your logs)
+    	root_logger = logging.getLogger()
+    	root_logger.addHandler(discord_handler)
 
-    # 	# Attach to discord.py logger (internal logs)
-    # 	discord_logger = logging.getLogger("discord")
-    # 	discord_logger.addHandler(discord_handler)
-    # 	discord_logger.setLevel(logging.INFO)
+    	# Attach to discord.py logger (internal logs)
+    	discord_logger = logging.getLogger("discord")
+    	discord_logger.addHandler(discord_handler)
+    	discord_logger.setLevel(logging.INFO)
 
-    # 	logging.debug("Discord logging handler attached!")
+    	logging.debug("Discord logging handler attached!")
 
     @bot.event
     async def on_ready():
